3ae5cc8a1e0102ca0a036c0602a7330687736f13
awakening <lucidsunlight@yandex.ru>
2023-04-10 17:40:53 +0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{
     fs::File,
-    io::{Read, Seek, Write},
-    path::Path, net::{UdpSocket, SocketAddr, Ipv4Addr, IpAddr}, env, mem::size_of
+    io::{Read, Seek, Write, self},
+    path::Path, net::{UdpSocket, SocketAddr, Ipv4Addr, IpAddr}, env, mem::size_of, process
 };
 
 use arbitrary_int::{u4, u9};
+use anyhow::Context;
 
 mod protocol;
 
@@ -16,7 +17,7 @@ struct BlockDevice {
 }
 
 impl BlockDevice {
-    fn new<P>(path: P) -> Self
+    fn new<P>(path: P) -> anyhow::Result<Self>
     where
         P: AsRef<Path>,
     {
@@ -34,40 +35,45 @@ impl BlockDevice {
                 options.write(false);
                 file = options.open(path);
             }
-            file.unwrap()
+            file.context("Failed to open device in read-only mode")?
         };
 
-        let file_size = file.seek(std::io::SeekFrom::End(0)).unwrap();
+        let file_size = file.seek(std::io::SeekFrom::End(0)).context("Failed to seek to the end of the device")?;
         let file_size_mb = file_size / (1000 * 1000);
         let file_size_mib = file_size / (1024 * 1024);
-        file.seek(std::io::SeekFrom::Start(0)).unwrap();
+        file.seek(std::io::SeekFrom::Start(0)).context("Failed to seek back to the startof the device")?;
 
         println!("Opened {path:?} as block device");
         println!(" - {}", if read_only { "read-only" } else { "read/write" });
         println!(" - size = {file_size_mb} MB / {file_size_mib} MiB");
 
-        Self {
+        Ok(Self {
             file,
             file_size,
-        }
+        })
     }
 
-    fn seek(&mut self, sector: u32) {
-        let offset = u64::from(sector) * 512;
+    fn seek(&mut self, sector: u32) -> io::Result<()> {
+        let offset = u64::from(sector) * u64::from(self.sector_size());
 
-        self.file.seek(std::io::SeekFrom::Start(offset)).unwrap();
+        self.file.seek(std::io::SeekFrom::Start(offset)).map(|_| ())
     }
 
-    fn read(&mut self, buf: &mut [u8]) {
-        self.file.read_exact(buf).unwrap();
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<()> {
+        self.file.read_exact(buf)
     }
 
-    fn write(&mut self, buf: &[u8]) {
-        self.file.write_all(buf).unwrap();
+    fn write(&mut self, buf: &[u8]) -> io::Result<()> {
+        self.file.write_all(buf)
     }
 
-    fn sector_size(&self) -> u16 { 512 }
-    fn sector_count(&self) -> u32 { (self.file_size / 512).try_into().unwrap() }
+    fn sector_size(&self) -> u16 {
+        512
+    }
+
+    fn sector_count(&self) -> u32 {
+        (self.file_size / u64::from(self.sector_size())).try_into().unwrap()
+    }
 }
 
 struct Server {
@@ -78,13 +84,14 @@ struct Server {
     blocks_per_socket: u16,
     socket: UdpSocket,
     write_size_left: usize,
+    write_rdma_valid: bool,
 }
 
 impl Server {
-    fn new(block_device: BlockDevice) -> Self {
+    fn new(block_device: BlockDevice) -> anyhow::Result<Self> {
         let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), UDPBD_PORT);
-        let socket = UdpSocket::bind(addr).unwrap();
-        socket.set_broadcast(true).unwrap();
+        let socket = UdpSocket::bind(addr).context("Failed to create UDP socket")?;
+        socket.set_broadcast(true).context("Failed to enable broadcast on UDP socket")?;
 
         let mut server = Server {
             block_device,
@@ -94,10 +101,11 @@ impl Server {
             blocks_per_socket: 0,
             socket,
             write_size_left: 0,
+            write_rdma_valid: false,
         };
         server.set_block_shift(5); // 128b blocks
 
-        server
+        Ok(server)
     }
 
     fn run(&mut self) {
@@ -107,15 +115,20 @@ impl Server {
         loop {
             let (_, addr) = self.socket.recv_from(&mut buf[..]).unwrap();
 
-            let header: &Header = bytemuck::from_bytes(&buf[..size_of::<Header>()]);
+            macro_rules! cast_buffer_as {
+                ($type:ty) => {
+                    bytemuck::from_bytes::<$type>(&buf[..size_of::<$type>()])
+                };
+            }
 
+            let header = cast_buffer_as!(Header);
             match header.command() {
                 Ok(cmd) => match cmd {
-                    Command::Info => self.handle_cmd_info(addr, bytemuck::from_bytes(&buf[..size_of::<InfoRequest>()])),
-                    Command::Read => self.handle_cmd_read(addr, bytemuck::from_bytes(&buf[..size_of::<ReadWriteRequest>()])),
-                    Command::Write => self.handle_cmd_write(bytemuck::from_bytes(&buf[..size_of::<ReadWriteRequest>()])),
-                    Command::WriteRdma => self.handle_cmd_write_rdma(addr, bytemuck::from_bytes(&buf)),
-                    cmd => println!("Expected request, got {cmd:?}")
+                    Command::Info => self.handle_cmd_info(cast_buffer_as!(InfoRequest), addr),
+                    Command::Read => self.handle_cmd_read(cast_buffer_as!(ReadWriteRequest), addr),
+                    Command::Write => self.handle_cmd_write(cast_buffer_as!(ReadWriteRequest)),
+                    Command::WriteRdma => self.handle_cmd_write_rdma(cast_buffer_as!(Rdma), addr),
+                    cmd => println!("Unexpected command: {cmd:?}")
                 },
                 Err(cmd) => println!("Unknown command: {cmd}"),
             };
@@ -134,9 +147,9 @@ impl Server {
 
     fn set_block_shift_sectors(&mut self, sectors: u16) {
         // Optimize for:
-        // 1 - the least number of network packets
-        // 2 - the largest block size (faster on the ps2)
-        let size = u32::from(sectors) * 512;
+        // - the least number of network packets
+        // - the largest block size (faster on the PS2)
+        let size = u32::from(sectors) * u32::from(self.block_device.sector_size());
         let packets_min  = (size + 1440 - 1) / 1440;
         let packets_128 = (size + 1408 - 1) / 1408;
         let packets_256 = (size + 1280 - 1) / 1280;
@@ -160,7 +173,7 @@ impl Server {
         self.set_block_shift(shift);
     }
 
-    fn handle_cmd_info(&mut self, addr: SocketAddr, req: &InfoRequest) {
+    fn handle_cmd_info(&mut self, req: &InfoRequest, addr: SocketAddr) {
         println!("UDPBD_CMD_INFO from {addr}");
 
         let reply = InfoReply {
@@ -173,12 +186,13 @@ impl Server {
         };
         let ser = bytemuck::bytes_of(&reply);
 
-        self.socket.send_to(ser, addr).unwrap();
+        if let Err(err) = self.socket.send_to(ser, addr) {
+            eprintln!("Failed to reply with UDPBD_CMD_INFO_REPLY to {addr}: {err}");
+        }
     }
 
-    fn handle_cmd_read(&mut self, addr: SocketAddr, req: &ReadWriteRequest) {
-        let sector_nr = req.sector_nr;
-        let sector_count = req.sector_count;
+    fn handle_cmd_read(&mut self, req: &ReadWriteRequest, addr: SocketAddr) {
+        let ReadWriteRequest { sector_nr, sector_count, .. } = *req;
 
         println!("UDPBD_CMD_READ(cmdId={}, startSector={}, sectorCount={})", req.header.command_id(), sector_nr, sector_count);
 
@@ -194,10 +208,13 @@ impl Server {
             data: [0; RDMA_MAX_PAYLOAD],
         };
 
+        let mut seeked = true;
+        if let Err(err) = self.block_device.seek(sector_nr) {
+            eprintln!("Failed to seek block device in UDPBD_CMD_READ for {addr}: {err}");
+            seeked = false;
+        }
+
         let mut blocks_left = sector_count * self.blocks_per_socket;
-
-        self.block_device.seek(sector_nr);
-
         while blocks_left > 0 {
             let block_count = if blocks_left > self.blocks_per_packet {
                 self.blocks_per_packet
@@ -210,33 +227,62 @@ impl Server {
             // read data from file
             let size = usize::from(block_count * self.block_size);
             let buf = &mut reply.data[..size];
-            self.block_device.read(buf);
+            if seeked {
+                if let Err(err) = self.block_device.read(buf) {
+                    eprintln!("Failed to read block device in UDPBD_CMD_READ for {addr}, zeroing: {err}");
+                    reply.data = [0; RDMA_MAX_PAYLOAD];
+                }
+            }
 
             let ser = bytemuck::bytes_of(&reply);
             let resp = &ser[..size_of::<Header>() + size_of::<BlockType>() + size];
 
             // send packet to PS2
-            self.socket.send_to(resp, addr).unwrap();
+            if let Err(err) = self.socket.send_to(resp, addr) {
+                eprintln!("Failed to reply with UDPBD_CMD_READ_RDMA to {addr}: {err}");
+            }
 
-            reply.header = reply.header.with_command_pkt(reply.header.command_pkt() + 1);
+            let next_cmd_pkt = reply.header.command_pkt() + 1;
+            reply.header = reply.header.with_command_pkt(next_cmd_pkt);
         }
     }
 
     fn handle_cmd_write(&mut self, req: &ReadWriteRequest) {
-        let sector_nr = req.sector_nr;
-        let sector_count = req.sector_count;
+        let ReadWriteRequest { sector_nr, sector_count, .. } = *req;
         println!("UDPBD_CMD_WRITE(cmdId={}, startSector={}, sectorCount={})", req.header.command_id(), sector_nr, sector_count);
 
-        self.block_device.seek(sector_nr);
-        self.write_size_left = usize::from(sector_count) * 512;
+        self.write_size_left = usize::from(sector_count) * usize::from(self.block_device.sector_size());
+
+        match self.block_device.seek(sector_nr) {
+            Ok(_) => {
+                self.write_rdma_valid = true;
+            },
+            Err(err) => {
+                eprintln!("Failed to seek to sector {sector_nr}: {err}");
+                self.write_rdma_valid = false;
+            },
+        }
     }
 
-    fn handle_cmd_write_rdma(&mut self, addr: SocketAddr, req: &Rdma) {
+    fn handle_cmd_write_rdma(&mut self, req: &Rdma, addr: SocketAddr) {
         let size = req.block_type.blocks_size();
         let data = &req.data[..size];
 
-        self.block_device.write(data);
-        self.write_size_left -= size;
+        #[allow(clippy::collapsible_if)]
+        if self.write_rdma_valid {
+            if self.block_device.write(data).is_err() {
+                eprintln!("Failed to write data to block device");
+            }
+        }
+
+        match self.write_size_left.checked_sub(size) {
+            Some(new_size) => self.write_size_left = new_size,
+            None => {
+                eprintln!("write_size_left wraparound at 0");
+                self.write_size_left = 0;
+            },
+        }
+
         if self.write_size_left == 0 {
             let reply = WriteReply {
                 header: Header::new_with_raw_value(0)
@@ -247,8 +293,9 @@ impl Server {
             };
             let ser = bytemuck::bytes_of(&reply);
 
-            self.socket.send_to(ser, addr).unwrap();
-
+            if let Err(err) = self.socket.send_to(ser, addr) {
+                eprintln!("Failed to reply with UDPBD_CMD_WRITE_DONE to {addr}: {err}");
+            };
         }
     }
 }
@@ -258,10 +305,9 @@ fn main() {
     if args.len() < 2 {
         println!("Usage:");
         println!("  {} <file>", args[0]);
-        return
+        process::exit(1);
     }
 
-    let bd = BlockDevice::new(&args[1]);
-    let mut server = Server::new(bd);
-    server.run();
+    let block_device = BlockDevice::new(&args[1]).unwrap();
+    Server::new(block_device).unwrap().run();
 }
